f7a2153184fc24d0eeb4b05b7f543911817435b6
lucasc-costa <62854155+lucasc-costa@users.noreply.github.com>
2023-01-13 22:10:45 -0300

    aula3

--- app/validacao.js
+++ app/validacao.js
@@ -2,11 +2,30 @@ function verificaSeOChutePossuiUmValorValido(chute){
     const numero =+chute
 
     if (chuteForInvalido(numero)){
-        console.log("valor invalido");
+        elementoChute.innerHTML += '<div>"valor invalido"</div>'
+        return
     }
 
     if (numeroForMaiorOuMenorQueOValorPermitido(numero)){
-        console.log(`valor inválido: o numero secreto precisa estar entre ${menorValor} e ${maiorValor}`);
+        elementoChute.innerHTML +=`
+        <div> valor inválido: Fale um numero entre ${menorValor} e ${maiorValor} </div>
+        `
+        return
+    }
+
+    if (numero === numeroSecreto) {
+        document.body.innerHTML=`
+            <h2>Você acertou!</h2>
+            <h3>O número secreto era ${numeroSecreto}</h3>
+        `
+    }else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+            <div>O número secreto é menor <i class="fa-solid fa-arrow-down"></i></div>
+        `
+    }else{
+        elementoChute.innerHTML += `
+            <div>O número secreto é maior <i class="fa-solid fa-arrow-up"></i></div>
+        `
     }
 }
 
